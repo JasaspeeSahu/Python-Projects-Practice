7f54dc4b95141d80e2ca4af7e29f873dfed170d5
JasaspeeSahu <jjasaspee@gmail.com>
2026-02-01 17:37:43 +0530

    made the preface

---
+++ Preface.py
@@ -0,0 +1,10 @@
+print("Hi this is my very first time buiddling something and tring to push it on GitHub. \n I know it's very basic and simple , I'm just trying things out.")
+print("this has 5 chapters to it which contain five different short stories")
+print('HAVE FUN !!')
+
+print('''      CHAPTER 1 : THE Magical Potion 
+      CHAPTER 2 : Trip to Mars
+      CHAPTER 3 : The Mystrey of The Missing Cookie
+      CHAPTER 4 : The Crazy Beach Day
+      CHAPTER 5 : The Timde Travelling Smartphone''')
+      
