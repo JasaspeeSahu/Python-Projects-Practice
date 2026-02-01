9389d2cad3e953ecf311a77fb0618ac0b77d67c9
JasaspeeSahu <jjasaspee@gmail.com>
2026-02-01 18:06:58 +0530

    wrote the first chapter

---
+++ chapter-1.py
@@ -0,0 +1,20 @@
+place = input('Your favorite place to visit') 
+adjective = input("Describe yourself in one word. can'ya shawty *smirks*") 
+name = input('What\'s your name dear Fineshyt ?') 
+verb = input('a weird action that you do regulary') 
+number = input("Now choose a number between 1 - 100") 
+liquid = input("And what does Fineshyt likes to drink ?") 
+magical_ingredient = input("It may sound weird but whats your fav spice?") 
+
+plural_noun  = input("and Finally what's your favorite feeling?") 
+
+
+
+
+
+
+
+print(f'''Once upon a time in the kingdom of {place}, there lived a {adjective} wizard named {name}.
+One day, {name} decided to brew a potion that could make anyone {verb}.
+To make it work, they needed {number} drops of {liquid} and a pinch of {magical_ingredient}.
+When the potion was ready, it started to {verb}, and suddenly the whole kingdom was filled with {plural_noun}.''')
